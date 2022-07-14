c1b46ad4fe3264a952fbc4261748d30c7554ae03
kovesdieszter <kovesdieszter@github.com>
2022-07-14 19:51:33 +0200

    feat: get rooms in service layer

--- src/main/java/com/codecool/hogwartshouses/service/RoomService.java
+++ src/main/java/com/codecool/hogwartshouses/service/RoomService.java
@@ -1,8 +1,11 @@
 package com.codecool.hogwartshouses.service;
 
+import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.DAO.RoomDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class RoomService {
@@ -10,4 +13,7 @@ public class RoomService {
     @Autowired
     private RoomDAO roomDAO;
 
+    public Set<Room> getRooms() {
+        return roomDAO.getRooms();
+    }
 }
