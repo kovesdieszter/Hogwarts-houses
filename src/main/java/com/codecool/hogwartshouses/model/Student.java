03e094bea3925116ce677156ecbdb96bd7bfc6dd
kovesdieszter <kovesdieszter@github.com>
2022-07-16 22:45:34 +0200

    feat: add PetType field

--- src/main/java/com/codecool/hogwartshouses/model/Student.java
+++ src/main/java/com/codecool/hogwartshouses/model/Student.java
@@ -1,5 +1,6 @@
 package com.codecool.hogwartshouses.model;
 
+import com.codecool.hogwartshouses.model.types.PetType;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,14 @@ import lombok.NoArgsConstructor;
 public class Student {
 
     private String name;
+    private PetType petType;
 
     public Student(String name) {
         this.name = name;
+    }
+
+    public Student(String name, PetType petType) {
+        this.name = name;
+        this.petType = petType;
     }
 }
