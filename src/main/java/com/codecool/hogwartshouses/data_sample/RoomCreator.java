1d163dd3db42c9ccdd31fc29f9424fdf61ca9995
kovesdieszter <kovesdieszter@github.com>
2022-07-16 22:45:55 +0200

    feat: initialize students with pet

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -2,6 +2,7 @@ package com.codecool.hogwartshouses.data_sample;
 
 import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.model.Student;
+import com.codecool.hogwartshouses.model.types.PetType;
 import com.codecool.hogwartshouses.service.DAO.RoomMemory;
 import com.codecool.hogwartshouses.service.DAO.StudentMemory;
 import org.springframework.stereotype.Component;
@@ -20,13 +21,19 @@ public class RoomCreator {
     }
 
     public void initialize() {
-        Student studentHermione = new Student("Hermione Granger");
-        Student studentDraco = new Student("Draco Malfoy");
+        Student studentHermione = new Student("Hermione Granger", PetType.CAT);
+        Student studentDraco = new Student("Draco Malfoy", PetType.OWL);
+        Student studentRon = new Student("Ron Weasley", PetType.RAT);
+        Student studentSteven = new Student("Steven Rat", PetType.RAT);
         studentMemory.addStudent(studentHermione);
         studentMemory.addStudent(studentDraco);
+        studentMemory.addStudent(studentRon);
+        studentMemory.addStudent(studentSteven);
 
         roomMemory.addRoom(new Room("Green", studentHermione));
         roomMemory.addRoom(new Room("Red", studentDraco));
+        roomMemory.addRoom(new Room("Black", studentRon));
+        roomMemory.addRoom(new Room("Grey", studentSteven));
         roomMemory.addRoom(new Room("White", true));
         roomMemory.addRoom(new Room("Purple", true));
     }
