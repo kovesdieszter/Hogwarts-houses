0408a5d1a71770f782329f8d18ac570782c8b96f
kovesdieszter <kovesdieszter@github.com>
2022-07-16 22:10:20 +0200

    feat: initialize students

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -1,7 +1,9 @@
 package com.codecool.hogwartshouses.data_sample;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
 import com.codecool.hogwartshouses.service.DAO.RoomMemory;
+import com.codecool.hogwartshouses.service.DAO.StudentMemory;
 import org.springframework.stereotype.Component;
 
 
@@ -9,16 +11,23 @@ import org.springframework.stereotype.Component;
 public class RoomCreator {
 
     private final RoomMemory roomMemory;
+    private final StudentMemory studentMemory;
 
-    public RoomCreator(RoomMemory roomMemory) {
+    public RoomCreator(RoomMemory roomMemory, StudentMemory studentMemory) {
         this.roomMemory = roomMemory;
+        this.studentMemory = studentMemory;
         initialize();
     }
 
     public void initialize() {
-        roomMemory.addRoom(new Room("Green"));
-        roomMemory.addRoom(new Room("Red"));
-        roomMemory.addRoom(new Room("Red"));
-    }
+        Student studentHermione = new Student("Hermione Granger");
+        Student studentDraco = new Student("Draco Malfoy");
+        studentMemory.addStudent(studentHermione);
+        studentMemory.addStudent(studentDraco);
 
+        roomMemory.addRoom(new Room("Green", studentHermione));
+        roomMemory.addRoom(new Room("Red", studentDraco));
+        roomMemory.addRoom(new Room("White", true));
+        roomMemory.addRoom(new Room("Purple", true));
+    }
 }
