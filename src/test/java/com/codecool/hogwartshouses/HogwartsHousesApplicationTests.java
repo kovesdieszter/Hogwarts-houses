da5ea1ca4921b32f3badd7fc9c859f5f22bf3ad7
kovesdieszter <kovesdieszter@github.com>
2022-09-21 15:56:29 +0200

    fix: fix tests

--- src/test/java/com/codecool/hogwartshouses/HogwartsHousesApplicationTests.java
+++ src/test/java/com/codecool/hogwartshouses/HogwartsHousesApplicationTests.java
@@ -1,12 +1,13 @@
 package com.codecool.hogwartshouses;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
+import com.codecool.hogwartshouses.model.types.PetType;
 import com.codecool.hogwartshouses.service.DAO.RoomMemory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,51 +20,71 @@ class HogwartsHousesApplicationTests {
 
 
 	@Test
-	void getRoomsTest(){
-		assertEquals(roomMemory.getRooms().size(), testRooms.size());
+	void getRoomsWithEmptyRoomsTest(){
+		assertEquals(0, roomMemory.getRooms().size());
+	}
+
+	@Test
+	void getRoomsWithRoomsTest(){
+		testRooms.addAll(Set.of(new Room(), new Room("Room1", false), new Room("Room2", true)));
+		assertEquals(3, roomMemory.getRooms().size());
 	}
 
 	@Test
 	void getRoomByIdTest() {
 		Room testRoom = new Room("Purple", true);
 		roomMemory.addRoom(testRoom);
-		System.out.println(roomMemory.getRooms());
 		assertEquals(roomMemory.getRoom(7), testRoom);
 	}
 
 	@Test
 	void addRoomTest(){
-		testRooms.add(new Room());
+		int sizeOfRooms = roomMemory.getRooms().size();
 		roomMemory.addRoom();
-		assertEquals(roomMemory.getRooms().size(), testRooms.size());
+		assertEquals(sizeOfRooms + 1, roomMemory.getRooms().size());
 	}
+
 
 	@Test
 	void deleteRoomTest(){
-		testRooms.add(new Room("Green", true));
-		testRooms.removeIf(room -> Objects.equals(room.getName(), "Green"));
-		roomMemory.deleteRoom(1);
-		assertEquals(roomMemory.getRooms().size(), testRooms.size());
+		testRooms.addAll(Set.of(new Room(), new Room("Room1", false), new Room("Room2", true)));
+		int sizeOfRooms = roomMemory.getRooms().size();
+		roomMemory.deleteRoom(19);
+		assertEquals(sizeOfRooms - 1, roomMemory.getRooms().size());
 	}
 
 	@Test
 	void updateRoomTest() {
 		roomMemory.addRoom(new Room("Green", true));
-		System.out.println(roomMemory.getRooms());
-		Room modifiedRoom = roomMemory.getRoom(10);
+		Room modifiedRoom = roomMemory.getRoom(15);
 		modifiedRoom.setName("TEST");
 		roomMemory.updateRoom(10, modifiedRoom);
-		assertEquals(roomMemory.getRoom(10).getName(), "TEST");
+		assertEquals(roomMemory.getRoom(15).getName(), "TEST");
 	}
 
 	@Test
-	void getAvailableRoomsTest() {
-		assertNotNull(roomMemory.getAvailableRooms());
+	void getAvailableRoomsWithEmptyRoomsTest() {
+		assertEquals(0, roomMemory.getAvailableRooms().size());
+	}
+	@Test
+	void getAvailableRoomsWithAvailableRoomsTest() {
+		testRooms.addAll(Set.of(new Room(), new Room("Room1", false), new Room("Room2", true)));
+		int sizeOfAvailableRooms = roomMemory.getAvailableRooms().size();
+		assertEquals(2, sizeOfAvailableRooms);
+	}
+
+	@Test
+	void getRoomsWithoutRatOwnersTest() {
+		assertEquals(0, roomMemory.getRoomsWithRatOwners().size());
 	}
 
 	@Test
 	void getRoomsWithRatOwnersTest() {
-		assertNotNull(roomMemory.getRoomsWithRatOwners());
+	testRooms.addAll(Set.of(
+			new Room(),
+			new Room("Room1", new Student("Ronny", PetType.RAT)),
+			new Room("Room2", new Student("Ron", PetType.CAT))));
+		assertEquals(1, roomMemory.getRoomsWithRatOwners().size());
 	}
 
 
